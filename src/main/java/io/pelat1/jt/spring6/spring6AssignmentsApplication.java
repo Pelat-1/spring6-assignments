699e4dd6ca309ae12dfc9961450a236706da6b6c
Pelat-1 <nicolo.rogai@outlook.com>
2023-04-23 17:42:08 +0200

    Spring Init

---
+++ src/main/java/io/pelat1/jt/spring6/spring6AssignmentsApplication.java
@@ -0,0 +1,13 @@
+package io.pelat1.jt.spring6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class spring6AssignmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(spring6AssignmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/pelat1/jt/spring6/spring6AssignmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pelat1.jt.spring6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class spring6AssignmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
