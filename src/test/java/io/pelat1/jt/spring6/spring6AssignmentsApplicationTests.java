9a6d3a29b76abd7aeb9230f56ef73aa9ba011671
Pelat-1 <nicolo.rogai@outlook.com>
2023-04-23 23:02:55 +0200

    Controller testing implemented

--- src/test/java/io/pelat1/jt/spring6/spring6AssignmentsApplicationTests.java
+++ src/test/java/io/pelat1/jt/spring6/spring6AssignmentsApplicationTests.java
@@ -1,13 +1,17 @@
 package io.pelat1.jt.spring6;
 
+import io.pelat1.jt.spring6.controller.FauxController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class spring6AssignmentsApplicationTests {
+	@Autowired
+	FauxController fauxController;
 
 	@Test
-	void contextLoads() {
+	void fauxDatasource() {
+		System.out.println(fauxController.getFauxDatasource());
 	}
-
 }
